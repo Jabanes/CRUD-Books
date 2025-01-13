b8b045770259339ff39a34a0753c475dfc9ccde7
Jabanes <erezhabani2003@gmail.com>
2025-01-13 18:22:08 +0200

    added logger (logs only if the app is not in debug=True)

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import sys
+from flask import Flask, request
 from loguru import logger
 from flask_cors import CORS
 from blueprints.home_bp import home_page
@@ -7,13 +8,19 @@ from blueprints.customers_bp import manage_customers
 from blueprints.loans_bp import manage_loans
 from db import init_db
 
-
 app = Flask(__name__)
 CORS(app)
-
-logger.add("logs/app.log", rotation="1 MB", retention="7 days", level="INFO")
-
 init_db()
+
+logger.remove()
+logger.add(sys.stdout, level="DEBUG", format="{time} {level} {message}")
+logger.add("app.log", rotation="1 day", retention="7 days", level="DEBUG", format="{time} {level} {message}")
+
+
+@app.before_request
+def log_request_info():
+    if not app.debug:
+        logger.info(f"Request: method={request.method}, path={request.path}, args={request.args}")
 
 app.register_blueprint(home_page)
 app.register_blueprint(manage_books)
